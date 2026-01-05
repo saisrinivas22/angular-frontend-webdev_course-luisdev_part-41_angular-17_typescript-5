1a06b1ff46313071aff50dee3dac50f738227e1c
Daniel Dantas <contatodanieldantasdev@gmail.com>
2026-01-04 23:30:58 -0300

    feat: adição do arquivo app.module.ts

---
+++ developments/DevFreelaAngular-26/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import {LOCALE_ID, DEFAULT_CURRENCY_CODE} from '@angular/core';
+import localePt from '@angular/common/locales/pt';
+import {registerLocaleData} from '@angular/common';
+
+registerLocaleData(localePt, 'pt');
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+  ],
+  providers: [
+    {
+        provide: LOCALE_ID,
+        useValue: 'pt'
+    },
+    {
+        provide:  DEFAULT_CURRENCY_CODE,
+        useValue: 'BRL'
+    },
+]
+,
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
